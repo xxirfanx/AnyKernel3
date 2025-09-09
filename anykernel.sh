af318f3920f724be215f77a7f462345b2609c7ff
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2025-09-09 07:34:32 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=Xposed hook by lua
 do.devicecheck=0
 do.modules=0
 do.systemless=0
